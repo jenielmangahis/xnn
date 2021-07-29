ae1fad3ce259ed6e7a5a7c84e99d93ea67da4e07
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-07-29 20:13:56 +0800

    off current period orders

--- olzdata/public/js/affiliate_dashboard.js
+++ olzdata/public/js/affiliate_dashboard.js
@@ -6,7 +6,7 @@
     const vm = new Vue({
         el: ".tool-container",
         data: {
-            dtCurrentPeriodOrders: null,
+            //dtCurrentPeriodOrders: null,
             dtGiftCards: null,
             defaultRank: 'Customer',
 
