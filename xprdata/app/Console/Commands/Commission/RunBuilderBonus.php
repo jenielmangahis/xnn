e9e613e44d2024edd33172180c016eef90a79f54
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-08-13 17:53:22 +0800

    Updates on the ftp

--- xprdata/app/Console/Commands/Commission/RunBuilderBonus.php
+++ xprdata/app/Console/Commands/Commission/RunBuilderBonus.php
@@ -29,7 +29,7 @@ class RunBuilderBonus extends Command
      *
      * @return void
      */
-    public function __construct(VolumesAndRanks $builderBonus)
+    public function __construct(BuilderBonus $builderBonus)
     {
         parent::__construct();
         $this->builderBonus = $builderBonus;
