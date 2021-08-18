40fe3bffb6b92a87cd40dc2eefe48a83813b19dd
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-08-19 00:15:21 +0800

    test

--- olzdata/pages/admin_clawback.pl
+++ olzdata/pages/admin_clawback.pl
@@ -300,7 +300,7 @@ print <<EOS;
                     <div class="form-group">
                         <div class="col-sm-12">
                             <div class="form-group form-check">
-                                <input type="checkbox" name="" class="form-check-input" id="is-sharing-link">
+                                <input type="checkbox" class="form-check-input" :value="1" id="is-sharing-link">
                                 <label class="form-check-label" for="exampleCheck1">Mark as Sharing link order</label>
                             </div>
                         </div>
