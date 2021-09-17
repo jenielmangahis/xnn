6f6ef550db7f24479a234d46219bbe99c158f103
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-09-18 00:16:59 +0800

    test

--- olzdata_live/public/admin_personal_retail_sale.js
+++ olzdata_live/public/admin_personal_retail_sale.js
@@ -17,7 +17,7 @@
             today: moment().format("YYYY-MM-DD"),
         },
         mounted() {
-            
+            this.initializeDataTables();
         },
         methods: {
             initializeDataTables() {
@@ -32,27 +32,21 @@
                         url: `${api_url}admin/personal-retail-sales/enrollment`,
                         data: function (d) {
                             d.start_date = _this.enrollment.filters.start_date;
-                            d.rank_id = _this.enrollment.filters.rank_id;
+                            d.end_date   = _this.enrollment.filters.end_date;                            
                         },
                     },
                     order: [[0, 'asc']],
-                    columns: [
-                        {
-                            data: 'user_id',
-                            render: function (data, type, row, meta) {
-                                let user_id = row.user_id;
-                                let member = row.member;
-                                return `${user_id}: ${member}`;
-                            }
-                        },
-                        {data: 'current_rank', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
-                        {data: 'paid_as_rank', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
-                        {data: 'prs', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
-                        {data: 'grs', className: "text-center"},
-                        {data: 'sponsored_qualified_representatives_count', className: "text-center"},
-                        {data: 'sponsored_leader_or_higher_count', className: "text-center"},                        
-                        {data: 'level', className: "text-center"},
-                        {data: 'rank_date', className: "text-center"},
+                    columns: [                        
+                        {data: 'top', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
+                        {data: 'id', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
+                        {data: 'member', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
+                        {data: 'enrolled_date', className: "text-center"},
+                        {data: 'affiliated_date', className: "text-center"},
+                        {data: 'email', className: "text-center"},
+                        {data: 'country', className: "text-center"},
+                        {data: 'sponsor_id', className: "text-center"},                        
+                        {data: 'sponsor', className: "text-center"},
+                        {data: 'prs', className: "text-center"},
                     ],
                     columnDefs: [
                         {responsivePriority: 1, targets: 0},
@@ -61,102 +55,6 @@
                         {responsivePriority: 4, targets: -4},
                     ]
                 });
-            },
-            initializeJQueryEvents() {
-
-                $('#nav-tab-report a[data-toggle="tab"]').on('shown.bs.tab',  (e) => {
-
-                    let tab = $(e.target).attr("href");
-
-                    if(tab === "#tree") {
-                        this.dtEnrollment.responsive.recalc();
-                    } else if(tab === "#new-highest-rank") {
-                        this.dtHighest.responsive.recalc();
-                    }
-
-                    this.dtEnrollment.columns.adjust();
-
-                })
-            },
-            viewEnrollment() {
-
-                this.enrollment.filters.start_date = this.enrollment.start_date;
-                this.enrollment.filters.rank_id = this.enrollment.rank_id;
-
-                this.dtEnrollment.clear().draw();
-                this.dtEnrollment.responsive.recalc();
-            },
-            viewHighest() {
-
-                this.highest.filters.start_date = this.highest.start_date;
-                this.highest.filters.end_date = this.highest.end_date;
-                this.highest.filters.rank_id = this.highest.rank_id;
-                this.highest.filters.is_all = this.highest.is_all;
-
-                this.dtHighest.clear().draw();
-                this.dtHighest.responsive.recalc();
-            },
-            getRanks() {
-
-                if (this.rankState === "fetching") return;
-
-                this.rankState = "fetching";
-                this.ranks = [];
-                this.enrollment.rank_id = "";
-                this.highest.rank_id = "";
-
-                client.get("common/ranks")
-                    .then(response => {
-                        this.ranks = response.data;
-                        this.rankState = "loaded";
-
-                    })
-                    .catch(error => {
-                        this.rankState = "error";
-                    })
-
-            },
-            getDownloadHighestLink() {
-                if (this.highest.downloadLinkState === "fetching") return;
-
-                this.highest.downloadLinkState = "fetching";
-                this.highest.downloadLink = "";
-
-                client.get("admin/rank-history/download-highest", {
-                    params: this.highest.filters
-                })
-                    .then(response => {
-                        this.highest.downloadLinkState = "loaded";
-                        this.highest.downloadLink = response.data.link;
-
-                        if (!!this.highest.downloadLink) {
-                            window.location = this.highest.downloadLink;
-                        }
-                    })
-                    .catch(error => {
-                        this.highest.downloadLinkState = "error";
-                    })
-            },
-            getDownloadEnrollmentLink() {
-                if (this.enrollment.downloadLinkState === "fetching") return;
-
-                this.enrollment.downloadLinkState = "fetching";
-                this.enrollment.downloadLink = "";
-
-                client.get("admin/rank-history/download-enrollment", {
-                    params: this.enrollment.filters
-                })
-                    .then(response => {
-                        this.enrollment.downloadLinkState = "loaded";
-                        this.enrollment.downloadLink = response.data.link;
-
-                        if (!!this.enrollment.downloadLink) {
-                            window.location = this.enrollment.downloadLink;
-                        }
-                    })
-                    .catch(error => {
-                        this.enrollment.downloadLinkState = "error";
-                    })
             },
         }
 
