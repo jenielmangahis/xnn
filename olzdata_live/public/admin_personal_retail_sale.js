58c0140c29c0cf350b5911b674aa7ff37c164ae8
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-09-18 02:05:17 +0800

    test

--- olzdata_live/public/admin_personal_retail_sale.js
+++ olzdata_live/public/admin_personal_retail_sale.js
@@ -44,7 +44,7 @@
                             d.memberId = _this.enrollment.filters.memberId;                   
                         },
                     },
-                    order: [[0, 'asc']],
+                    order: [[8, 'desc']],
                     columns: [                        
                         {data: 'rownum', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
                         {data: 'user_id', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
