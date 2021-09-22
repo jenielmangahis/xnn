5772bce9b67f059d4966e37e67993b87af701064
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-09-22 21:34:12 +0800

    test

--- olzdata_live/public/admin_personal_retail_sale.js
+++ olzdata_live/public/admin_personal_retail_sale.js
@@ -7,34 +7,32 @@
 
     const vm = new Vue({
         el: "#personal-retail-sale",
-        data: function () {
-            return {
-                autocompleteUrl: `${api_url}common/autocomplete/members`,
-                enrollment: {                    
-                    filters: {
-                        start_date: null,
-                        end_date: null,
-                        transaction_start_date : null,
-                        transaction_end_date : null,
-                        memberId: null,
-                        prs_500_above : '',
-                    },
+        data: {
+            autocompleteUrl: `${api_url}common/autocomplete/members`,
+            enrollment: {                    
+                filters: {
+                    start_date: null,
+                    end_date: null,
+                    transaction_start_date : null,
+                    transaction_end_date : null,
+                    memberId: null,
+                    prs_500_above : '',
                 },
-                csvPersonalRetail: {
-                    filters: {
-                        start_date: null,
-                        end_date: null,
-                        transaction_start_date : null,
-                        transaction_end_date : null,
-                        memberId: null,
-                        prs_500_above : '',
-                    },
+            },
+            csvPersonalRetail: {
+                filters: {
+                    start_date: null,
+                    end_date: null,
+                    transaction_start_date : null,
+                    transaction_end_date : null,
+                    memberId: null,
+                    prs_500_above : '',
+                },
 
-                    downloadLink: "",
-                    downloadLinkState: "loaded",
-                },
-                today: moment().format("YYYY-MM-DD"),
-            }
+                downloadLink: "",
+                downloadLinkState: "loaded",
+            },
+            today: moment().format("YYYY-MM-DD"),
         },
         mounted() {
             this.initializeDataTables();
@@ -125,7 +123,7 @@
                 this.dtEnrollment.responsive.recalc();
             },
             getDownloadPersonalRetail() {
-                
+
                 this.csvPersonalRetail.filters.start_date = $('#enrollment-start-date').val();
                 this.csvPersonalRetail.filters.end_date = $('#enrollment-end-date').val(); 
                 this.csvPersonalRetail.filters.prs_500_above = this.enrollment.filters.prs_500_above;
