82496c8d6f95cbb1f84ce237e64575cbe583bc8a
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-10-07 23:13:09 +0800

    updated DFO for NXM transaction live

--- dfodata/pages/admin_transactions_report.pl
+++ dfodata/pages/admin_transactions_report.pl
@@ -8,153 +8,111 @@ print <<EOS;
 <link rel="stylesheet" href="$commission_engine_api_url/css/admin_transactions_report.css?v=1" />
 
 <div id="transactions-report" class="tool-container tool-container--default">
-
     <div class="row">
         <div class="col-md-12">
-            <h4 class="admin-money-title">Transaction Report</h4>
+            <h4>Transactions</h4>
+            <hr />
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="form-horizontal">
+                <div class="form-group">
+                    <div class="form-sub-group col-md-6 col-lg-6">
+                        <label for="date-from">Date From</label>
 
-    <div class="put-mobile-lr-padding">
-
-        <div class="panel panel-default">
-
-            <div class="panel-body">
-
-                <div class="row margin-top">
-                    <div class="col-md-6">
-                        <div class="form-horizontal">
-
-                            <div class="form-group">
-                                <div class="form-sub-group col-md-6 col-lg-6">
-                                    <label for="date-from">From</label>
-
-                                    <div class='input-group start-date' >
-                                        <input type='text' class="form-control required" id='date-from' />
-                                        <span class="input-group-addon date-from-icon">
-                                            <i class="glyphicon glyphicon-calendar"></i>
-                                        </span>
-                                    </div>
-                                    <span class="help-block"></span>
-                                </div>
-                                <div class="form-sub-group col-md-6 col-lg-6">
-                                    <label for="date-to">To</label>
-                                    <div class='input-group end-date' >
-                                        <input type='text' class="form-control required" id='date-to' />
-                                        <span class="input-group-addon date-to-icon">
-                                            <i class="glyphicon glyphicon-calendar"></i>
-                                        </span>
-                                    </div>
-                                    <span class="help-block"></span>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="form-sub-group col-md-6 col-lg-6">
-                                    <label for="status">Status:</label>
-                                    <select name="status" id="status" class="form-control">
-                                        <option value="All" selected>All</option>
-                                        <option value="Approved">Approved</option>
-                                        <option value="Declined">Declined</option>
-                                        <option value="Error">Error</option>
-                                        <option value="Failed">Failed</option>
-                                    </select>
-                                    <span class="help-block"></span>
-                                </div>
-                                <div class="form-sub-group col-md-6 col-lg-6">
-                                    <button class="btn btn-primary pull-right flat btn-block" id="button-generate-report">View Report</button>
-                                </div>
-                            </div>
-
+                        <div class='input-group start-date' >
+                            <input type='text' class="form-control required" id='date-from' />
+                            <span class="input-group-addon date-from-icon">
+                                <i class="glyphicon glyphicon-calendar"></i>
+                            </span>
                         </div>
-
+                        <span class="help-block"></span>
                     </div>
-
-                    <div class="col-md-6">
-                        <table class="table table-striped table-bordered dt-responsive nowrap" style="width: 100%;">
-                            <thead class="table__header table__header--bg-primary">
-                            <tr class="table__row">
-                                <td colspan="2">Summary</td>
-                            </tr>
-                            </thead>
-                            <tbody class="table__body">
-                                <tr>
-                                    <td>Credit Card</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td>Ledger</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td>Gift Card</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td>Coupons</td>
-                                    <td></td>
-                                </tr>
-            				</tbody>
-                        </table>
+                    <div class="form-sub-group col-md-6 col-lg-6">
+                        <label for="date-to">Date To</label>
+                        <div class='input-group end-date' >
+                            <input type='text' class="form-control required" id='date-to' />
+                            <span class="input-group-addon date-to-icon">
+                                <i class="glyphicon glyphicon-calendar"></i>
+                            </span>
+                        </div>
+                        <span class="help-block"></span>
                     </div>
-
                 </div>
-
-                <div class="row margin-top margin-bottom padding-left-right-15">
-
-                    <div class="col-md-12 dashes-container-2">
-                        <div class="row">
-
-                            <div class="col-md-4 no-padding-left">
-                                <div class="form-group" id="download-report-links">
-                                    
-                                    <a id="link-download-csv" class="btn btn-info btn-block margin-left-minus-30" href="#">
-                                        Download Transaction Report
-                                    </a>
-                                    
-                                </div>
-                            </div>
-
-                            <div class="col-md-4">
-                            </div>
-
-                            <div class="col-md-4">
-                                <div class="total-style-controller">
-                                    <h5 class="admin-money-total"><strong>Total</strong></h5>
-                                </div>
-                            </div>
-
-                        </div>
-
+                <div class="form-group">
+                    <div class="form-sub-group col-md-6 col-lg-6">
+                        <label for="status">Status:</label>
+                        <select name="status" id="status" class="form-control">
+                            <option value="All" selected>All</option>
+                            <option value="Approved">Approved</option>
+                            <option value="Declined">Declined</option>
+                            <option value="Error">Error</option>
+                            <option value="Failed">Failed</option>
+                        </select>
+                        <span class="help-block"></span>
                     </div>
-
+                    <div class="form-sub-group col-md-6 col-lg-6">
+                        <button class="btn btn-primary pull-right flat btn-block" id="button-generate-report">GO</button>
+                    </div>
                 </div>
+                <div class="form-group" id="download-report-links" style="display: none;">
 
-                <div class="row put-mobile-lr-padding">
-                    <div class="table-responsive">
-                        <div class="col-md-12">
-                            <table id="table-admin-transactions" class="table table-striped table-bordered dt-responsive nowrap" style="width: 100%;">
-                                <thead class="table__header table__header--bg-primary">
-                                <tr class="table__row">
-                                    <td class="table__cell">Commission Type</td>
-                                    <td class="table__cell">Commission Period</td>
-                                    <td class="table__cell">Invoice</td>
-                                    <td class="table__cell">Purchaser</td>
-                                    <td class="table__cell">Order Date</td>
-                                    <td class="table__cell">Level</td>
-                                    <td class="table__cell">CV</td>
-                                    <td class="table__cell">Percentage</td>
-                                    <td class="table__cell">Amount</td>
-                                </tr>
-                                </thead>
-                                <tbody class="table__body">
-                				</tbody>
-                            </table>
-                        </div>
+                    <div class="form-sub-group col-md-6 col-lg-6">
+                        <a style="margin-bottom:10px!important;" id="link-download-csv" class="btn btn-primary flat btn-block" href="#">
+                            <span class="glyphicon glyphicon-th-list" aria-hidden="true">  </span> Download CSV
+                        </a>
                     </div>
                 </div>
             </div>
-
+        </div>
+        <div class="col-md-6">
+            <table id="table-admin-totals" class="table table-striped table-bordered dt-responsive nowrap" style="width: 100%;">
+                <thead class="table__header table__header--bg-primary">
+                <tr class="table__row">
+                    <th class="table__cell">Totals</th>
+                    <th class="table__cell">All</th>
+                    <th class="table__cell">Approved</th>
+                    <th class="table__cell">Declined</th>
+                    <th class="table__cell">Error</th>
+                    <th class="table__cell">Failed</th>
+                </tr>
+                </thead>
+                <tbody class="table__body">
+				</tbody>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <table id="table-admin-transactions" class="table table-striped table-bordered dt-responsive nowrap" style="width: 100%;">
+                <thead class="table__header table__header--bg-primary">
+                <tr class="table__row">
+                    <th class="table__cell">ORDER ID</th>
+                    <th class="table__cell">Purchaser</th>
+                    <th class="table__cell">Sponsor</th>
+                    <th class="table__cell">Product</th>
+                    <th class="table__cell">CV</th>
+                    <th class="table__cell">Volume</th>
+                    <th class="table__cell">Date
+                        <br>Purchase</th>
+                    <th class="table__cell">Price
+                        <br>Paid</th>
+                    <th class="table__cell">Credited</th>
+                    <th class="table__cell">Status</th>
+                    <th class="table__cell">Order
+                        <br>Type</th>
+                    <th class="table__cell">Payment
+                        <br>Type</th>
+                    <th class="table__cell">Phone</th>
+                    <th class="table__cell">Gift Card</th>
+                    <th class="table__cell">Coupon</th>
+                    <th class="table__cell">Ledger</th>
+                </tr>
+                </thead>
+                <tbody class="table__body">
+				</tbody>
+            </table>
         </div>
     </div>
 </div>
@@ -167,7 +125,7 @@ print <<EOS;
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.full.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
-<script src="$commission_engine_api_url/js/admin_transactions_report.js?v=1.3"></script>
+<script src="$commission_engine_api_url/js/admin_transactions_report.js?v=1.4"></script>
 
 EOS
 1;
