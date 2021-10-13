a43233a5006a3d4b6ea111293e14c72c400ebe76
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-10-14 01:20:21 +0800

    test

--- dfodata/pages/admin_withdrawal_request.pl
+++ dfodata/pages/admin_withdrawal_request.pl
@@ -187,6 +187,7 @@ print <<EOS;
 <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js" integrity="sha256-4iQZ6BVL4qNKlQ27TExEhBN1HFPvAvAMbFavKKosSWQ=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/eonasdan-bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.min.js"></script>
 <script src="$commission_engine_api_url/js/admin_withdrawal_request.js?v=1.1"></script>
 
 EOS
