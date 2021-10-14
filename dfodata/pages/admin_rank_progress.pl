7406de94d5082a82f6d43c2fe7b05dc21b368a62
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-10-14 19:17:51 +0800

    test

--- dfodata/pages/admin_rank_progress.pl
+++ dfodata/pages/admin_rank_progress.pl
@@ -5,17 +5,16 @@ print <<EOS;
 
 
 <div id="rank-progress" class="rank-progress tool-container tool-container--default" v-cloak>
+    
     <div class="row">
-        <div class="col-md-12">
-            <h4 class="admin-money-title">$rank_title Progress</h4>
-        </div>
+        <h4 class="admin-money-title">$rank_title Progress</h4>
     </div>
 
     <div class="put-mobile-lr-padding">
 
-        <div class="panel panel-default">
+        <div class="card with-border-no-radius">
 
-            <div class="panel-body">
+            <div class="card-body">
 
                 <div class="row margin-top">
                     <div class="col-md-4">
@@ -55,30 +54,30 @@ print <<EOS;
                                 true-value="1"
                                 false-value="0">
                             <label for="is_all_below">
-                                <strong>Show all $rank_title_plural below the selected $rank_title<//strong>
+                                <strong>Show all $rank_title_plural below the selected $rank_title</strong>
                             </label>
                         </div>
                     </div>
 
                     <div class="col-md-4">
-                        <div class="col-md-8">
-                            <label>&nbsp;</label><br>
-                            <button
-                                type="button"
-                                class="btn btn-primary btn-block"
-                                v-on:click.prevent="view"
-                            >
-                                View Report
-                            </button>
-                        </div>
-                        <div class="col-md-4"></div>
+                        
+                        <label>&nbsp;</label><br>
+                        <button
+                            type="button"
+                            class="btn btn-primary btn-block"
+                            v-on:click.prevent="view"
+                        >
+                            View Report
+                        </button>
+                        
                     </div>
 
                 </div>
 
                 <div class="row margin-top">
-                    <div class="table-responsive">
-                        <div class="col-md-12 padding-left-right">
+                    <div class="col-md-12">
+                        <div class="table-responsive">
+                        
                             <table id="table-rank-progress" class="table table-striped table-bordered dt-responsive nowrap" style="width:100%">
                                 <thead class="table__header table__header--bg-primary">
                                 <tr class="table__row">
@@ -94,13 +93,16 @@ print <<EOS;
                                 <tbody class="table__body">
                                 </tbody>
                             </table>
+                            
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
 </div>
+
 <script src="$commission_engine_api_url/js/app.js?v=$app_js_version"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js"></script>
