1ed52d1d9b632949313f1705f7a966d8fdcb2372
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-10-15 22:48:29 +0800

    test

--- dfodata/public/js/affiliate_rank_progress.js
+++ dfodata/public/js/affiliate_rank_progress.js
@@ -39,78 +39,14 @@
                     },
                     order: [[2, 'asc']],
                     columns: [
-                        {
-                            data: 'user_id',
-                            render: function (data, type, row, meta) {
-                                let user_id = row.user_id;
-                                let member = row.member;
-                                return `${user_id}: ${member}`;
-                            }
-                        },
-                        {
-                            data: 'rank_id',
-                            render: function (data, type, row, meta) {
-                                return row.current_rank;
-                            }
-                        },
-                        {
-                            data: 'paid_as_rank_id',
-                            render: function (data, type, row, meta) {
-                                return row.paid_as_rank;
-                            }
-                        },
-                        {data: 'referral_points', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
-                        {data: 'coach_points', className: "text-center"},
-                        {data: 'organization_points', className: "text-center"},
-                        {data: 'team_group_points', className: "text-center"},
-                        {data: 'preferred_customer_count', className: "text-center"},
-                        {data: 'influencer_count', className: "text-center"},
-                        {data: 'silver_influencer_count', className: "text-center"},
-                        {data: 'gold_influencer_count', className: "text-center"},
-                        {data: 'platinum_influencer_count', className: "text-center"},
-                        {data: 'diamond_influencer_count', className: "text-center"},
-                        {
-                            data: 'is_active',
-                            className: "text-center",
-                            render: function (data, type, row, meta) {
-
-                                if (+row.is_active) {
-                                    return `<span class="label label-success">Yes</span>`;
-                                }
-
-                                return `<span class="label label-warning">No</span>`;
-                            }
-                        },
-                        {data: 'level', className: "text-center"},
-                        {
-                            data: 'sponsor_id',
-                            render: function (data, type, row, meta) {
-                                let sponsor_id = row.sponsor_id;
-                                let sponsor = row.sponsor;
-                                return `${sponsor_id}: ${sponsor}`;
-                            }
-                        },
-                        {
-                            data: null,
-                            orderable: false,
-                            render: function (data, type, row) {
-                                let needs = row.needs;
-                                let list = '';
-
-                                for (let i = 0; i < needs.length; i++) {
-                                    let n = needs[i];
-
-                                    if(typeof n.html !== "undefined") {
-                                        list += `<li>${n.html}</li>`
-                                    } else {
-                                        list += `<li><strong>${n.value}</strong> ${n.description}</li>`
-                                    }
-                                }
-
-                                return `<ul class="list-unstyled">${list}</ul>`;
-                            }
-                        },
-                    ],
+                        { data: 'member' },
+                        { data: 'level' },
+                        { data: 'current_rank' },
+                        { data: 'paid_as_rank' },
+                        {data: 'pv', className: "text-center"}, // render: $.fn.dataTable.render.number(',', '.', 2, '$')
+                        {data: 'l1v', className: "text-center"},
+                        {data: 'l1vneeds', className: "text-center"}                        
+                    ]
                     columnDefs: [
 
                         {responsivePriority: 1, targets: 0},
