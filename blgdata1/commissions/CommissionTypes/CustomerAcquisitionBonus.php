9c028c13e4143dd8a6061129ad26a66dfe1ad59e
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-10-20 23:50:09 +0800

    test

--- blgdata1/commissions/CommissionTypes/CustomerAcquisitionBonus.php
+++ blgdata1/commissions/CommissionTypes/CustomerAcquisitionBonus.php
@@ -30,7 +30,7 @@ class CustomerAcquisitionBonus extends CommissionType
             $sponsor_id   = $order['sponsor_id'];
             $is_valid     = true;
             if( $order['influencer_level'] == 1 ){
-                $totalOrders = $this->totalOrdersPass60Days($order['sponsor_id']); //Has at least 1 sale in the last 60 days (if Free Influencer)
+                $totalOrders = $this->totalOrdersLast60Days($order['sponsor_id']); //Has at least 1 sale in the last 60 days (if Free Influencer)
                 if( $totalOrders['total_orders'] > 0 ){
                     $is_valid = true;
                 }else{
@@ -62,7 +62,7 @@ class CustomerAcquisitionBonus extends CommissionType
 
     }
 
-    public function totalOrdersPass60Days( $sponsor_id )
+    public function totalOrdersLast60Days( $sponsor_id )
     {
         $last_60_days = date('Y-m-d', strtotime('-60 days'));
         $today = date('Y-m-d');
@@ -99,7 +99,7 @@ class CustomerAcquisitionBonus extends CommissionType
             JOIN cm_daily_ranks dr ON dr.user_id = t.sponsor_id AND dr.rank_date = '$end_date'
             JOIN transaction_products tp ON tp.transaction_id = t.transaction_id
             WHERE t.transaction_date BETWEEN '$start_date' AND '$end_date' 
-                AND (dr.rank_id = 1 OR dr.influencer_level = 1)
+                AND (dr.rank_id >= 1 OR dr.influencer_level >= 1)
                 AND dr.is_active = 1
                 AND FIND_IN_SET(tp.shoppingcart_product_id,'13,14,15')            
                 AND t.`type` = 'product' ";
