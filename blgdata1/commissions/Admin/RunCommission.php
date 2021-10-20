466a833edf8f197573fe2fbbb22179e8810a27df
Jeniel Mangahis <jeniel.mangahis@gmail.com>
2021-10-21 01:04:33 +0800

    test

--- blgdata1/commissions/Admin/RunCommission.php
+++ blgdata1/commissions/Admin/RunCommission.php
@@ -563,7 +563,7 @@ class RunCommission
         $period = CommissionPeriod::find($commission_period_id);
         $background_worker_logger = new BackgroundWorkerLogger(storage_path(static::LOG_PATH), $background_worker_id, $background_worker_process_id);
         $payout_repository = new PayoutRepository();
-        echo "Com ID " . $commission_type_id;exit;
+        echo "Config ID " . config('commission.commission-types.customer-acquisition-bonus');exit;
         switch (+$commission_type_id) {
             case config('commission.commission-types.fast-start-bonus'):
                 return new FastStartBonus($period, $background_worker_logger, $payout_repository);
